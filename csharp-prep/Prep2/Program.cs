2b6c0e3f22612fce0e1001c587deb89e56d86611
Nels Buhrley <nelsbuhrley@Nels-MacBook.localdomain>
2025-01-10 00:43:05 -0700

    Wrote code for Prep 1 & 2 and did the streach chalenges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What Grade did you get? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade = "F";
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        
+        if (grade > 95)
+        {
+            letterGrade = "A";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+        else if (grade % 10 >= 7)
+        {
+            letterGrade += "+";
+        }
+        else if (grade % 10 <= 3)
+        {
+            letterGrade += "-";
+        }
+
+        Console.WriteLine($"You got a {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
+// add plus or minus
+        
+
     }
 }
